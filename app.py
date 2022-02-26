0e9d3d1378e9fd9ec21972f92cd4fc487d4fc670
RowenTey <94209928+RowenTey@users.noreply.github.com>
2022-02-26 15:00:42 +0800

    Divided parts to work on

--- app.py
+++ app.py
@@ -3,18 +3,39 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Hack4Pan",
-    page_icon="https://cdn-icons-png.flaticon.com/128/3094/3094918.png",
+    page_icon="https://icon-library.com/images/career-icon-png/career-icon-png-0.jpg",
     initial_sidebar_state='collapsed'
 ) 
     
 st.image("https://cdn.substack.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fa4b8647a-09e8-4576-95de-e622dcc38d72_1280x720.jpeg")
-st.title("Welcome to our iNTUition 2022 App!")
+st.title("Looking for a job?")
+
 # with st.expander("How To Use"):
 #     st.markdown("First, type in the **ticker** symbol you're looking for.")  
 #     st.markdown("Next, click the arrow icon on the top left corner. This opens up a sidebar.")             
 #     st.markdown(" Select the date you wish to start predicting the stock price from.")
 #     st.markdown("Give a moment for the model to process your input.")              
-#     st.markdown("And that's it! The prediction will appear magically before your eyes. It's that **easy**!")            
+#     st.markdown("And that's it! The prediction will appear magically before your eyes. It's that **easy**!")
+
+### Kai Seong
+
+
+###
+
+### Shao Wei
+
+
+###
+
+### Horstann
+
+
+###
+
+### Ci Hui
+
+
+###
 
 # footer          
 st.markdown("*By **Kai Seong**, **Shao Wei**, **Ci Hui** & **Horstann** for iNTUition 2022.*")
