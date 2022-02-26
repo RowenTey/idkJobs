32a589453991725ddfcdbf600c6470cb6c3f7b11
RowenTey <94209928+RowenTey@users.noreply.github.com>
2022-02-26 10:16:13 +0800

    Added "hello"

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ st.set_page_config(
 ) 
     
 st.image("https://cdn.substack.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fa4b8647a-09e8-4576-95de-e622dcc38d72_1280x720.jpeg")
-st.title("Welcome to the Stock App!")
-with st.expander("How To Use"):
-    st.markdown("First, type in the **ticker** symbol you're looking for.")  
-    st.markdown("Next, click the arrow icon on the top left corner. This opens up a sidebar.")             
-    st.markdown(" Select the date you wish to start predicting the stock price from.")
-    st.markdown("Give a moment for the model to process your input.")              
-    st.markdown("And that's it! The prediction will appear magically before your eyes. It's that **easy**!")            
+st.title("Welcome to our iNTUition 2022 App!")
+# with st.expander("How To Use"):
+#     st.markdown("First, type in the **ticker** symbol you're looking for.")  
+#     st.markdown("Next, click the arrow icon on the top left corner. This opens up a sidebar.")             
+#     st.markdown(" Select the date you wish to start predicting the stock price from.")
+#     st.markdown("Give a moment for the model to process your input.")              
+#     st.markdown("And that's it! The prediction will appear magically before your eyes. It's that **easy**!")            
+
+hello = 100
 
 # footer          
-st.markdown("*By **Kai Seong** & **Horstann** for Hack4Pan 2022.*")
+st.markdown("*By **Kai Seong**, **Shao Wei**, **Ci Hui** & **Horstann** for iNTUition 2022.*")
