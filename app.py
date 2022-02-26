91948b7060199b2f3153d052d07319d6168b8173
RowenTey <94209928+RowenTey@users.noreply.github.com>
2022-02-26 09:23:17 +0800

    Initial Commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st 
+
+
+st.set_page_config(
+    page_title="Hack4Pan",
+    page_icon="https://cdn-icons-png.flaticon.com/128/3094/3094918.png",
+    initial_sidebar_state='collapsed'
+) 
+    
+st.image("https://cdn.substack.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fa4b8647a-09e8-4576-95de-e622dcc38d72_1280x720.jpeg")
+st.title("Welcome to the Stock App!")
+with st.expander("How To Use"):
+    st.markdown("First, type in the **ticker** symbol you're looking for.")  
+    st.markdown("Next, click the arrow icon on the top left corner. This opens up a sidebar.")             
+    st.markdown(" Select the date you wish to start predicting the stock price from.")
+    st.markdown("Give a moment for the model to process your input.")              
+    st.markdown("And that's it! The prediction will appear magically before your eyes. It's that **easy**!")            
+
+# footer          
+st.markdown("*By **Kai Seong** & **Horstann** for Hack4Pan 2022.*")
