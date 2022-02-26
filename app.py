a99ee2c65e06cdf0e34e3d2e8e0d612f4ee83b4a
RowenTey <94209928+RowenTey@users.noreply.github.com>
2022-02-26 16:32:28 +0800

    Added roles to be selected

--- app.py
+++ app.py
@@ -6,14 +6,19 @@ st.set_page_config(
     page_icon="https://icon-library.com/images/career-icon-png/career-icon-png-0.jpg",
     initial_sidebar_state='collapsed'
 ) 
-    
+
+roles = ["Sofware Engineer", "Full-Stack Developer", "Front-End Developer", "Back-End Developer", 
+         "Data Engineer", "Cloud Engineer", "Game Developer", "Blockchain Engineer",
+         "Mobile Developer"]
+
 st.image("https://cdn.substack.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fa4b8647a-09e8-4576-95de-e622dcc38d72_1280x720.jpeg")
-st.title("Looking for a job?")
-st.write("Check out the sidebar to find out more :)")
-st.sidebar.subheader("What role are you looking for?")
-st.sidebar.selectbox()
+st.title("Looking for a job in the tech industry?")
+st.subheader("Check out the sidebar to find out more :)")
+st.sidebar.subheader("What role  are  you looking for?")
+user_input = st.sidebar.selectbox("Select a role",roles)
 
 ### Kai Seong
+
 
 
 ###
